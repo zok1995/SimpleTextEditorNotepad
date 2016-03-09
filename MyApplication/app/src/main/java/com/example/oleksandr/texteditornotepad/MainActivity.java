5a91f43aa294dea93bc2d896663ca686533c41b6
Oleksandr Malakhovskyi <zokzok95@gmail.com>
2016-03-09 22:35:59 +0200

    Edited menu working!

--- MyApplication/app/src/main/java/com/example/oleksandr/texteditornotepad/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/oleksandr/texteditornotepad/MainActivity.java
@@ -1,13 +1,15 @@
 package com.example.oleksandr.texteditornotepad;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
     private EditText mEditTextMainField;
 
     @Override
@@ -19,7 +21,8 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main,menu);
         return super.onCreateOptionsMenu(menu);
     }
 
