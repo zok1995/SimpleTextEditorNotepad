fc43446e6bd957172e944342c4e137d0d197cf9e
Oleksandr Malakhovskyi <zokzok95@gmail.com>
2016-03-09 22:06:21 +0200

    Menu shows, button clear are working

--- MyApplication/app/src/main/java/com/example/oleksandr/texteditornotepad/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/oleksandr/texteditornotepad/MainActivity.java
@@ -2,12 +2,35 @@ package com.example.oleksandr.texteditornotepad;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText mEditTextMainField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mEditTextMainField = (EditText) findViewById(R.id.editText);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.actionClear:
+                mEditTextMainField.setText("");
+                Toast.makeText(getApplicationContext(),"Empty!",Toast.LENGTH_SHORT).show();
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
