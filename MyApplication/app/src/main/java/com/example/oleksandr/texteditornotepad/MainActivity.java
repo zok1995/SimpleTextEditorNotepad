47d3479ab039e04b5a3a1db45fa9140edb5053b2
Oleksandr Malakhovskyi <zokzok95@gmail.com>
2016-03-10 14:17:42 +0200

    Changing text color  ready

--- MyApplication/app/src/main/java/com/example/oleksandr/texteditornotepad/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/oleksandr/texteditornotepad/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.oleksandr.texteditornotepad;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -15,6 +17,7 @@ import android.widget.Toast;
 public class MainActivity extends Activity {
     private EditText mEditTextMainField;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,8 +29,38 @@ public class MainActivity extends Activity {
     protected void onResume() {
         super.onResume();
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-        float fSize = Float.parseFloat(sharedPreferences.getString("Size", "20"));
+        float fSize = Float.parseFloat(sharedPreferences.getString(getString(R.string.size), "20"));
         mEditTextMainField.setTextSize(fSize);
+
+        String regular = sharedPreferences.getString(getString(R.string.preference_style),"");
+        int iTypeface = Typeface.NORMAL;
+        if (regular.contains("Bold"))
+            iTypeface += Typeface.BOLD;
+
+        if (regular.contains("Italics"))
+            iTypeface +=Typeface.ITALIC;
+
+        mEditTextMainField.setTypeface(null,iTypeface);
+
+        if (sharedPreferences.getBoolean(getString(R.string.preference_color_red),false)){
+            mEditTextMainField.setTextColor(Color.RED);
+        }
+
+        if (sharedPreferences.getBoolean(getString(R.string.preference_color_white),false)){
+            mEditTextMainField.setTextColor(Color.WHITE);
+        }
+
+        if (sharedPreferences.getBoolean(getString(R.string.preference_color_black),false)){
+            mEditTextMainField.setTextColor(Color.BLACK);
+        }
+
+        if (sharedPreferences.getBoolean(getString(R.string.preference_color_green),false)){
+            mEditTextMainField.setTextColor(Color.GREEN);
+        }
+
+
+
+
     }
 
     @Override
